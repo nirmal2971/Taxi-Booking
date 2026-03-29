93df38cce1e898d485a32240eb69ebd122d54ec1
NIRMAL RAJ M <126080219+nirmal2971@users.noreply.github.com>
2026-03-29 16:25:09 +0530

    Frontne dissue

--- taxi-frontend/src/services/api.jsx
+++ taxi-frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://taxi-backend-awjx.onrender.com",
+  baseURL: "https://taxi-backend-awjx.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
